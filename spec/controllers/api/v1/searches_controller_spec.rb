6cc1e6a10b553614ecef97f6ad9a7ecfefa9d36a
Xiaofeng Yuan <yuan.xiaofeng@gmail.com>
2016-12-12 21:58:03 +0800

    improve searches_controller spec

--- spec/controllers/api/v1/searches_controller_spec.rb
+++ spec/controllers/api/v1/searches_controller_spec.rb
@@ -1,30 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::SearchesController, type: :controller do
-  let(:seller) { create :seller, name: 'Barca Club' }
-  let!(:product) { create :product, name: 'Messi is Barca for ever', seller: seller }
-  let(:json) { JSON.parse response.body }
-
-  before do
-    get :index, params: { q: 'barca' }
+  def import *args
+    StoreIndex.import! *args
   end
 
-  after do
-    Chewy.massacre
-  end
+  before { StoreIndex.purge! }
 
-  it { should respond_with 200 }
+  after { Chewy.massacre }
 
-  it 'returns hash of result' do
-    expect(json).to be_a Hash
-  end
+  describe '#index' do
+    let(:seller)  { create :seller, name: 'Barca Club' }
+    let(:product) { create :product, name: 'Messi is Barca for ever', seller: seller }
+    let(:json)    { JSON.parse response.body }
 
-  it 'has `data` as root key' do
-    expect(json).to have_key 'data'
-  end
+    before do
+      import seller: seller, product: product
+      get :index, params: { q: 'barca' }
+    end
 
-  it 'responds with searched records' do
-    expect(json['data'].map {|item| item['attributes']['title']}).to include seller.name
-    expect(json['data'].map {|item| item['attributes']['title']}).to include product.name
+    it { should respond_with 200 }
+
+    it 'returns hash of result' do
+      expect(json).to be_a Hash
+    end
+
+    it 'has `data` as root key' do
+      expect(json).to have_key 'data'
+    end
+
+    it 'responds with searched records' do
+      expect(json['data'].map {|item| item['attributes']['title']}).to include seller.name
+      expect(json['data'].map {|item| item['attributes']['title']}).to include product.name
+    end
   end
 end
